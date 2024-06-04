f67d409c26e40cda9b046dbbf9d74e5aa17ff34b
RococoKid <sara.r.kalkstein@gmail.com>
2024-06-03 22:40:04 -0400

    move footer and nav outside of landingpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import LandingPage from './LandingPage/LandingPage';
+import Nav from './LandingPage/Nav';
+import Footer from './LandingPage/Footer'
 
 const App = () => {
     return (<div>
+        <Nav/>
         Welcome to OOTO
         <LandingPage/>
+        <Footer/>
     </div>);
 };
-
 
 export default App;
--- client/src/LandingPage/LandingPage.jsx
+++ client/src/LandingPage/LandingPage.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Nav from './Nav';
-import Footer from './Footer';
 import Testimonials from './Testimonials';
 import Benefits from './Benefits';
 import Main from './Main';
@@ -9,12 +7,10 @@ import StayConnected from './StayConnected';
 const LandingPage = () => {
     return (
         <div>
-            <Nav/>
             <Main/>
             <Benefits/>
             <Testimonials/>
             <StayConnected/>
-            <Footer/>
         </div>
     )
 }
